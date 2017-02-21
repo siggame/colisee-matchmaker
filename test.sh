de936c52d4bd988e81a6f7381fdba3104fa9d891
Matthew Qualls <mqggd@mst.edu>
2017-02-21 17:46:02 -0600

    Changed docker to pull Stable DB

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker pull siggame/colisee-db
+docker pull siggame/colisee-db:stable
 docker rm --force matchmaker-db
-docker run --name matchmaker-db --publish 5432:5432 --detach siggame/colisee-db
+docker run --name matchmaker-db --publish 5432:5432 --detach siggame/colisee-db:stable
 
 npm run test
 
