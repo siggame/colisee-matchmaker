7debcc30bf5ae9e3487213d15906b0552bbac7a0
Unknown <russleyshaw@gmail.com>
2017-09-03 14:06:15 -0500

    Remove common

--- src/app.ts
+++ src/app.ts
@@ -29,5 +29,3 @@ export class App {
     async poll(): Promise<void> {
     }
 }
-
-
--- src/common/logger.ts
+++
@@ -1,8 +0,0 @@
-import * as winston from "winston";
-
-exports = new (winston.Logger)({
-    transports: [
-      new (winston.transports.Console)(),
-      new (winston.transports.File)({ filename: "app.log" })
-    ]
-});
