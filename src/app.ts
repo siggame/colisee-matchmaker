48ec25d616373d18742eca0a37e4648ad305e2b4
user404d <conduffql@gmail.com>
2017-09-09 16:05:08 -0500

    Add /start, /stop, and /queue endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import { db } from "@siggame/colisee-lib";
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import { InternalServerError } from "http-errors";
 import * as winston from "winston";
+
+import { Matchmaker } from "./matchmaker";
+import { PORT } from "./vars";
+
+const app = express();
+const mm = new Matchmaker();
 
 winston.configure({
     transports: [
@@ -11,8 +21,32 @@ winston.configure({
     ],
 });
 
-import { Matchmaker } from "./matchmaker";
+app.get("/start", bodyParser.json(), (req, res) => {
+    try {
+        winston.info(req.body);
+        mm.start();
+        res.end();
+    } catch (e) {
+        winston.error(e);
+        throw new InternalServerError(e);
+    }
+});
 
-const mm = new Matchmaker();
+app.get("/stop", bodyParser.json(), (req, res) => {
+    try {
+        mm.stop();
+        res.end();
+    } catch (e) {
+        winston.error(e);
+        throw new InternalServerError(e);
+    }
+});
 
-mm.start();
+app.get("/queued", async (req, res) => {
+    res.json(await db.connection("games").where({ status: "queued" }).then(db.rowsToGames));
+});
+
+app.listen(PORT, () => {
+    mm.start();
+    winston.info(`Listening on port ${PORT}...`);
+});
