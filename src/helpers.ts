3f89707aa731d6cfeaf656cefa9eb1134f99bf8b
user404d <conduffql@gmail.com>
2017-09-11 15:38:22 -0500

    Add documentation for helpers and rename pair creation

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,11 @@
+/**
+ * Creates a permutation (ie. random shuffle) of the input list.
+ * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+ * 
+ * @export
+ * @param {any[]} list 
+ * @returns {any[]}
+ */
 export function permute(list: any[]) {
     const permutation = Array.from(list);
     let window = permutation.length;
@@ -14,7 +22,17 @@ export function permute(list: any[]) {
     return permutation;
 }
 
-export function generateDerangedPairs<T>(list: T[]): [T, T][] {
+/**
+ * Create pairs from the input list by pairing each item with the
+ * next item in the list. The final item will be paired with the first
+ * item.
+ * 
+ * @export
+ * @template T 
+ * @param {T[]} list 
+ * @returns {[T, T][]} 
+ */
+export function createPairs<T>(list: T[]): [T, T][] {
     // make pair for each element from the original list and
     // the list after being rotated by one to the right
     return list.map((item, i): [T, T] => [item, list[(i + 1) % list.length]]);
