ee05a5d23d7a21a24b807ae8f0c5f3a11163570d
user404d <conduffql@gmail.com>
2018-02-19 23:02:58 -0600

    Update to use same env var name

--- src/vars.ts
+++ src/vars.ts
@@ -3,5 +3,5 @@ import { defaultTo, toNumber } from "lodash";
 // Application
 export const PORT: number = defaultTo<number>(toNumber(process.env.PORT), 8080);
 export const REPLICATIONS: number = defaultTo<number>(toNumber(process.env.REPLICATIONS), 4);
-export const SCHED_INTERVAL: number = defaultTo<number>(toNumber(process.env.INTERVAL), 1000);
-export const SCHED_MAX: number = defaultTo<number>(toNumber(process.env.MAX), 30);
+export const SCHED_INTERVAL: number = defaultTo<number>(toNumber(process.env.SCHED_INTERVAL), 1000);
+export const SCHED_MAX: number = defaultTo<number>(toNumber(process.env.SCHED_MAX), 30);
